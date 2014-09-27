1c11e442d4dec7c376d1f86bea71e427cb63d258
Rohithyeravothula <yeravothula@iitj.ac.in>
2014-09-28 00:32:06 +0530

    first commit

---
+++ insomia2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,m;
+        scanf("%d %d",&n,&m);
+        int a[n][m];
+        for(i=0;i<n;i++)
+        for(j=0;j<m;j++)
+    }
+
+}
